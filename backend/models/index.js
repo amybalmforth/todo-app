d5431b5cca02a092672e937b49a473b25707c1db
Amy Balmforth <amy.balmforth@gmail.com>
2021-01-30 14:49:14 +0000

    set up db

---
+++ backend/models/index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/todo-app', {
+  keepAlive: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+mongoose.set('debug', true)
+mongoose.Promise = Promise;
