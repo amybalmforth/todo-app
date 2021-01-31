2c4334b0c1cb8793566d9776251d4e5dc0ebc2f0
Amy Balmforth <amy.balmforth@gmail.com>
2021-01-31 18:15:41 +0000

    change ~ for :

--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -7,9 +7,9 @@ const TodoList = ({ todos, deleteTodo, completeTodo }) => {
       {todos.map(todo => {
         return (
           <li key={todo._id} id="list">
-            <span className={todo.completed ? 'completed' : ''}>{todo.task + ' ~ '}
+            <span className={todo.completed ? 'completed' : ''}>{todo.task + ' : '}
             {todo.description + ' '}
-            {todo.date ? ' ~ ' + todo.date.slice(0, -14) : todo.date} </span>
+            {todo.date ? ' : ' + todo.date.slice(0, -14) : todo.date} </span>
             <button name="complete" id="complete" onClick={() => completeTodo(todo._id)}>complete</button>
             <button name="delete" id="delete" onClick={() => deleteTodo(todo._id)}>delete</button>
           </li>
